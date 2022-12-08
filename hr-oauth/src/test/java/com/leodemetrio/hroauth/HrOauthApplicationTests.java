1906f1e66fcde7b97cd712a9170a808a853965cc
leo-demetrio <leocdemetrio@gmail.com>
2022-12-08 16:25:15 -0300

    Porject hr-oauth created

---
+++ hr-oauth/src/test/java/com/leodemetrio/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leodemetrio.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
