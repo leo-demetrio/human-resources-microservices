1e3bcd914cfd815baecd7d030141cc62ac3c5f0a
leo-demetrio <leocdemetrio@gmail.com>
2022-12-08 19:30:36 -0300

    Login, Jwt token

--- hr-oauth/src/main/java/com/leodemetrio/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/leodemetrio/hroauth/services/UserService.java
@@ -4,12 +4,15 @@ import com.leodemetrio.hroauth.entities.User;
 import com.leodemetrio.hroauth.feignclients.UserFeignClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static Logger logger = LoggerFactory.getLogger(UserService.class);
     private final UserFeignClient userFeignClient;
@@ -22,6 +25,17 @@ public class UserService {
         if(user == null) {
             logger.error("Email not found");
             throw new IllegalArgumentException("Email not found");
+        }
+        logger.info(email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userFeignClient.findByEmail(email).getBody();
+        if(user == null) {
+            logger.error("Email not found");
+            throw new UsernameNotFoundException("Email not found");
         }
         logger.info(email);
         return user;
